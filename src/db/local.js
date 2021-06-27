315731674d9ea10b4f91c232797ef8b619edfd5f
Lucas Smith <lucas@lucasesmith.com>
2021-06-27 15:04:50 +0000

    Integrate cleanup of no longer applicable messages.

--- src/db/local.js
+++ src/db/local.js
@@ -10,16 +10,16 @@ export const write = (id, data) => {
 export const readAll = () => {
     const db = {};
     return new Promise((resolve, reject) => {
-        fs.readdir("./database", (err, files) => {
-            if(err){
+        fs.readdir(dbPath, (err, files) => {
+            if (err) {
                 reject(err);
             }
             const fileCount = files.length;
-            for(const file of files){
+            for (const file of files) {
                 fs.readFile(`${dbPath}/${file}`, (data) => {
                     const parsedData = JSON.parse(data);
                     db[file] = parsedData;
-                    if(Object.keys(db).length === fileCount){
+                    if (Object.keys(db).length === fileCount) {
                         resolve(db);
                     }
                 })
@@ -27,3 +27,17 @@ export const readAll = () => {
         });
     })
 }
+
+const msDay = 1000 * 60 * 60 * 24;
+const msHour = 1000 * 60 * 60;
+setInterval(() => {
+    const time = new Date().getTime();
+    fs.readdir(dbPath, (err, files) => {
+        for (const file of files) {
+            const fileName = Number(file);
+            if (fileName + msDay < time) {
+                fs.rm(`${dbPath}/${fileName}`, { recursive: true, force: true });
+            }
+        }
+    })
+}, msHour);
